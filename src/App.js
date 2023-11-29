72023e97e3bb6d38ea09f300d207f4945ff57110
ojongclinton <sheyenoh123@gmail.com>
2023-11-29 07:29:38 +0100

    initial release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      fetch("http://127.0.0.1:8080/api/products", { method: "GET" })
+        .then(async (data) => {
+          let response = await data.json();
+          console.log(response);
+          setProducts(response);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+    fetchProducts();
+  }, []);
+  // console.log(products);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr" }}>
+        {products.map((product) => {
+          return (
+            <div
+              style={{ margin: "20px", border: "1px solid" }}
+              className="singleProduct"
+            >
+              <h3>{product.name}</h3>
+              <img
+                src={product.imageUrl}
+                alt=""
+                style={{ height: "300px", width: "100%" }}
+              />
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h4>Price {product.amount + " " + product.currency}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
